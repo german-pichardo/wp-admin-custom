f6554080b3bb77e773388b830d61598fd06c535c
gpichardo <gpichardob@gmail.com>
2018-05-08 12:27:04 +0200

    rename index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var download = require('gulp-download');
-var packageJSON = require('./package.json');
-
+// var packageJSON = require('./package.json');
+// TODO remove
+var packageJSON = {
+    'version': '4.9.5'
+};
 var source = [
 	'http://core.svn.wordpress.org/tags/' + packageJSON.version + '/wp-admin/css/colors/_admin.scss',
 	'http://core.svn.wordpress.org/tags/' + packageJSON.version + '/wp-admin/css/colors/_variables.scss',
 	'http://core.svn.wordpress.org/tags/' + packageJSON.version + '/wp-admin/css/colors/_mixins.scss',
 ];
 
-gulp.task('default', function () {
-	return download(source).pipe(gulp.dest('src/'));
-});
+function downloadSource () {
+    return download(source).pipe(gulp.dest('src/'));
+}
+
+module.exports = downloadSource();
