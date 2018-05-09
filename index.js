4def30575a7cd89ed181cb6ae8addf18e825057d
gpichardo <gpichardob@gmail.com>
2018-05-09 22:40:05 +0200

    format

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+'use strict';
+
 var gulp = require('gulp');
 var download = require('gulp-download');
 var packageJSON = require('./package.json');
 
 var source = [
-	'http://core.svn.wordpress.org/tags/' + packageJSON.version + '/wp-admin/css/colors/_admin.scss',
-	'http://core.svn.wordpress.org/tags/' + packageJSON.version + '/wp-admin/css/colors/_variables.scss',
-	'http://core.svn.wordpress.org/tags/' + packageJSON.version + '/wp-admin/css/colors/_mixins.scss'
+    'http://core.svn.wordpress.org/tags/' + packageJSON.version + '/wp-admin/css/colors/_admin.scss',
+    'http://core.svn.wordpress.org/tags/' + packageJSON.version + '/wp-admin/css/colors/_variables.scss',
+    'http://core.svn.wordpress.org/tags/' + packageJSON.version + '/wp-admin/css/colors/_mixins.scss'
 ];
 
-function downloadSource () {
+function downloadSource() {
     return download(source).pipe(gulp.dest('src/'));
 }
 
